030211bb2becf66bc8faa1c332ee2464eeb95cd7
wuch298-cmd <wuch298@gmail.com>
2026-07-18 15:59:27 +0800

    chore: initialize Spring Boot project

---
+++ src/main/java/com/wuch298/studyflow/StudyFlowApplication.java
@@ -0,0 +1,13 @@
+package com.wuch298.studyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuch298/studyflow/StudyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuch298.studyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
